06a9eef6c8b53d5ae6810777b8cb82df78351f91
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2023-11-30 18:21:57 +0000

    Variable and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 18231
+let accountEmail = "rastogidhruv28@gmail.com"
+var accountPassword = "2003"
+accountCity = "Agra"
+
+//accountId=2 //not allowed
+//console.log(accountId);
+accountEmail="nitigya282@gmail.com"
+accountPassword="2002"
+accountCity="Bengaluru"
+console.table([accountEmail,accountPassword,accountCity]);
+
+/*
+Prefer not to use var due to issues in block and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all Js code as newer version
+
+//alert(3+3)  //we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Dhruv")
+
+let name = "Dhruv"
+let age =20
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint for bigger stock related numbers
+//string =>""
+//boolean => true/false
+//undefined => value not assigned
+//null => standalone value
+//symbol => unique
+
+
+//object
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
