a34231baceb87c04c889c8d7d6d73d7d8a50af82
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2023-12-01 13:11:58 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let age="21abs"
+console.log(typeof age);
+console.log(typeof(age));
+
+let valueInNumber= Number(age)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "21" => 21
+// "21abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+
+let isLoggedin=1
+let booleanloggedin=Boolean(isLoggedin)
+console.log(booleanloggedin)
+
+//1=>true; 0=>false
+//"" =>false;
+// "dhruv"=>true
+
+let number=18
+let stringNumber=String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
