18852b8be5ae2f9114f02c1c20c50101c096828a
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2023-12-02 12:59:09 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let age="21abs"
+/*let age="21abs"
 console.log(typeof age);
 console.log(typeof(age));
 
@@ -25,3 +25,37 @@ let stringNumber=String(number)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+/*********************************Operations ************************************* */
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)*/
+
+let s1= "hello"
+let s2= " Dhruv"
+let s3= s1+s2
+console.log(s1+s2)
+
+//Complex situations
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")  //Not adopt this kind of code for corporate , just for understanding ECMA basics
+
+console.log(+true)  //output =>1
+console.log(+"")    //output =>0
+
+//Always focus on simple and readable codes rather than showing off mind compelxity
+//let num1,num2,num3
+//num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
