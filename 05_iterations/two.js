a27e7cc208b8c40b105cc013740bd64387af086f
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2023-12-13 17:15:52 +0000

    For,while and dowhile loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+
+let index =0;
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index=index+2
+}
+
+let myArr= ['Dennish','Suresh','Swami']
+let arr=0
+while(arr<myArr.length){
+    console.log(`Value is ${myArr[arr]}`);
+    arr=arr+1
+}
+
+let score=1
+do {
+    console.log(`Score is ${score}`)   //kaam pehle condition baad mei check hoti hai
+    score++;
+} while (score<=10);
